46ca49a94ec7bb9aafac43a54c7f504288a3e243
Lucas de S. Santos <110189896+LukkazZ12@users.noreply.github.com>
2022-12-19 20:37:23 -0300

    Update Questao4.cs

--- Questao4.cs
+++ Questao4.cs
@@ -6,7 +6,7 @@ namespace Questao4
     {
         static void Main(string[] args)
         {
-            static int funcaoRetornaQuantitadePOV(decimal porcentagem, int totalNegociado)
+            public static int funcaoRetornaQuantitadePOV(decimal porcentagem, int totalNegociado)
                 => Convert.ToInt32(Math.Floor(Decimal.Divide(totalNegociado * porcentagem, Decimal.Subtract(1, porcentagem))));
         }
     }
